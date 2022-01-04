d7648fac6b7c871a0c73a1f2112362d73e99f9a1
YosmanProgramer <Yosman.higuita028@gmail.com>
2022-01-04 18:55:27 -0500

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    enabled: process.env.HUGO_ENVIRONMENT === "production",
-    content: ["./layouts/**/*.html"],
+    enabled: process.env.HUGO_ENVIRONMENT === 'production',
+    content: ['./layouts/**/*.html'],
     options: {
       safelist: [],
     },
@@ -11,11 +11,16 @@ module.exports = {
     extend: {
       fontFamily: {},
       backgroundImage: {},
-      colors: {},
+      colors: {
+        'purple': '#603E85',
+        'purple-dark': '#472F62',
+        'purple-light': '#9D7DC2',
+        'grey': '#EAEAEA',
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
